62a94f0c978b358ac9130d8aa8c73711221f1055
divyagangigunta123 <gangigunta.divya9@gmail.com>
2015-04-22 16:05:56 +0530

    commit updated

--- hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/parse/PcapMapper.java
+++ hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/parse/PcapMapper.java
@@ -9,51 +9,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class PcapMapper extends Mapper<KeyWritable, ValueWritable, KeyWritable, ValueWritable> {
 	
-	//private static final KeyWritable KEYWRITABLE = new KeyWritable();
+	
 	
 	private static final KeyWritable KEY = new KeyWritable();
 	private static final ValueWritable VALUE = new ValueWritable();
-//	private static final PacketBean BEAN = new PacketBean(); 
+
 
 	public void map(KeyWritable key, ValueWritable value, Context context) throws IOException,
 			InterruptedException {
 		
 	
-
-//		 Packet packet = (Packet) value.get();
-		
-		
-		 
-//		 int sPort_repetition = 0;
-//		 int dPort_repetition = 0;
-		
-		/* List<String> srcIp = new ArrayList<String>();
-		 List<String> sPort = new ArrayList<String>();
-		 List<String> destIp = new ArrayList<String>();
-		 List<String> dPort = new ArrayList<String>();
-		 List<String> time = new ArrayList<String>();
-		 List<Integer> bytes = new ArrayList<Integer>();
-		
-		*/
-//		 if (value != null) {
-//		 srcIp.add(value.getSourceIp().toString());
-//		 destIp.add(value.getServerIp().toString());
-//		 sPort.add(value.getSrcPort().toString());
-//		 dPort.add(value.getDstPort().toString());
-//		 time.add(value.getTimeStamp().toString());
-//		 bytes.add(Integer.valueOf(value.getDownloadTraffic().toString()));
-//		
-//		 }
-		
-//		 int sum = 0;
-//		
-//		 for(int val : bytes) {
-//		
-//		 sum += val;
-//		
-//		 }
-//		
-		
 		Text client_Ip = key.getClientIp();
 		Text timeStamp = key.getTimestamp();
 		
@@ -74,10 +39,7 @@ public class PcapMapper extends Mapper<KeyWritable, ValueWritable, KeyWritable, 
 		VALUE.setDstPort(destPort);
 		VALUE.setTimeStamp(timestamp);
 		VALUE.setDownloadTraffic(traffic);
-		
-//		BEAN.setSrcPort(srcPort.toString());
-//		BEAN.setDestPort(destPort.toString());
-//		BEAN.setBytes_transfered(traffic.toString());
+	
 
 		context.write(KEY,VALUE);
 	}
